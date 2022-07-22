d2e4654fb6d7d9f864b6b17f849952a00688ef69
Len87G <Eleniac@github.com>
2022-07-21 21:22:10 -0600

    updated comments

--- Assets/script.js
+++ Assets/script.js
@@ -14,11 +14,11 @@ generateBtn.addEventListener("click", writePassword);
 //function determining the length of the password 
 function generatePassword() {
 
-  //prompts user to enter 8-12 characters to determine the length of their password
+  //prompts user to enter 8-128 characters to determine the length of their password
   var length = prompt(
     "Please write the number of characters you would like your password to be. Please keep the length you select between 8-128."
   );
-  //where selected characters category choices will be stored to generate the password at the end
+  //where the selected length and characters will be stored
   var selectedCharacters = [];
 
   // if the length is less than 8 or greater than 128 
@@ -35,7 +35,7 @@ function generatePassword() {
   var uppercaseCharacters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O",
     "P","Q","R","S","T","U","V","W","X","Y","Z",];
 
-// if statement stating if they pick true or "okay" the contents of uppercaseCharacters will be added to the selected characters pool
+// if they pick true or "okay" the contents of uppercaseCharacters will be added to the selected characters pool
   if (uppercase) {
     selectedCharacters=selectedCharacters.concat(uppercaseCharacters);
   }
@@ -43,7 +43,7 @@ function generatePassword() {
   var lowercase = confirm("Do you want to use a lowercase letter?");
   var lowercaseCharacters = ["a", "b", "c", "d", "e", "f" ,"g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s" ,"t", "u", "v", "w", "x", "y", "z"]
 
-// if statement stating if they pick true or "okay" the contents of lowercaseCharacters will be added to the selected characters pool
+// if they pick true or "okay" the contents of lowercaseCharacters will be added to the selected characters pool
   if (lowercase) {
     selectedCharacters=selectedCharacters.concat(lowercaseCharacters);
   }
@@ -52,7 +52,7 @@ function generatePassword() {
 
   var numericCharacters = ["1", "2", "3","4","5", "6", "7", "8", "9", "10"]
 
-// if statement stating if they pick true or "okay" the contents of numericharacters will be added to the selected characters pool
+// if they pick true or "okay" the contents of numericharacters will be added to the selected characters pool
   if (numericValue) {
     selectedCharacters=selectedCharacters.concat(numericCharacters);
   }
@@ -60,10 +60,11 @@ function generatePassword() {
   var special = confirm("Do you want to use special characters?");
   var specialCharacters = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "{", "}", "[", "]", ":", ";", "'", ".", "<", ".", "/", "?", "|"]
 
-// if statement stating if they pick true or "okay" the contents of specialcharacters will be added to the selected characters pool
+// if they pick true or "okay" the contents of specialcharacters will be added to the selected characters pool
   if (special) {
     selectedCharacters=selectedCharacters.concat(specialCharacters);
   }
+  //if the user clicks false for all the characters it will bring them to the start to try again
 if (special=== false && numericValue=== false && lowercase=== false && uppercase=== false) {
 alert("Please start over you did not enter the correct criteria to create the password.Thank you!");
 generatePassword()
@@ -75,7 +76,7 @@ for (var i = 0; i<length; i++) {
  var randomchar = selectedCharacters[index]
  
  password=password+randomchar
-} 
+} //password funtion
 return password;
 }
 
